c788cc0e5349483a2b7e925ce1847ba2b448e5aa
Yash Gaur <yashgaur2525@gmail.com>
2023-03-19 22:51:03 +0530

    projects added

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -1,5 +1,8 @@
 import Work1 from '../../assets/movie_world.png';
-
+import Work2 from '../../assets/portfolio.png';
+import Work3 from '../../assets/weather__app.png';
+import Work4 from '../../assets/notes_taking_website.png';
+import Work5 from '../../assets/to_do_list.png';
 
 export const projectsData = [
     {
@@ -12,31 +15,35 @@ export const projectsData = [
     },
     {
         id:2,
-        image: Work1,
-        title: 'Movie World 2',
+        image: Work2,
+        title: 'Portfolio Website',
         category: 'Frontend',
-        demoLink: ''
+        demoLink: 'https://yash-portfolio.onrender.com/',
+        github: 'https://github.com/THEYASHGAUR/ReactJS-Portfolio'
     },
     {
         id:3,
-        image: Work1,
-        title: 'backend',
-        category: 'Backend',
-        demoLink: ''
+        image: Work3,
+        title: 'Weather App',
+        category: 'frontend',
+        demoLink: 'https://weather-app-new-six.vercel.app/',
+        github: 'https://github.com/THEYASHGAUR/Weather-App'
     },
     {
         id:4,
-        image: Work1,
-        title: 'ui ',
+        image: Work4,
+        title: 'Notes Taking Website ',
         category: 'UI / UX',
-        demoLink: ''
+        demoLink: 'https://notes-taking-application.netlify.app/',
+        github: 'https://github.com/THEYASHGAUR/Notes-taking-Website'
     },
     {
         id:5,
-        image: Work1,
-        title: 'ux',
+        image: Work5,
+        title: 'To Do List App',
         category: 'UI / UX',
-        demoLink: ''
+        demoLink: 'https://to-do-list-by-yashgaur.netlify.app/',
+        github:'https://github.com/THEYASHGAUR/To-Do-List-using-ReactJS'
     }
     
 ]
