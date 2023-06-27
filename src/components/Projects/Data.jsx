4db983763a1a8a58162c11c44d4d7a9eef74921d
Yash Gaur <yashgaur2525@gmail.com>
2023-06-27 22:20:19 +0530

    updated projects section

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -2,16 +2,18 @@ import Work1 from '../../assets/movie_world.png';
 import Work2 from '../../assets/portfolio.png';
 import Work3 from '../../assets/weather__app.png';
 import Work4 from '../../assets/notes_taking_website.png';
-import Work5 from '../../assets/to-do-app.png';
+// import Work5 from '../../assets/to-do-app.png';
 import Work6 from '../../assets/qr-code-2.png';
 import Work7 from '../../assets/speech_to_text_convertor.png';
+import Work8 from '../../assets/cli.jpg';
+import Work9 from '../../assets/quote.png';
 
 export const projectsData = [
     {
         id:1,
         image: Work1,
         title: 'Movie World',
-        category: 'Frontend',
+        category: 'API Based',
         demoLink: 'https://movie-world-by-yash.netlify.app/',
         github: 'https://github.com/THEYASHGAUR/Movie-World'
     },
@@ -35,20 +37,20 @@ export const projectsData = [
         id:4,
         image: Work4,
         title: 'Notes Taking Website ',
-        category: 'UI / UX',
+        category: 'Frontend',
         demoLink: 'https://notes-taking-application.netlify.app/',
         github: 'https://github.com/THEYASHGAUR/Notes-taking-Website'
     },
+    // {
+    //     id:5,
+    //     image: Work5,
+    //     title: 'To Do List App',
+    //     category: 'backend',
+    //     demoLink: 'https://to-do-list-by-yashgaur.netlify.app/',
+    //     github:'https://github.com/THEYASHGAUR/To-Do-List-using-ReactJS'
+    // },
     {
-        id:5,
-        image: Work5,
-        title: 'To Do List App',
-        category: 'Frontend',
-        demoLink: 'https://to-do-list-by-yashgaur.netlify.app/',
-        github:'https://github.com/THEYASHGAUR/To-Do-List-using-ReactJS'
-    },
-    {
-        id:5,
+        id:6,
         image: Work6,
         title: 'QR Code Generator',
         category: 'API Based',
@@ -56,12 +58,28 @@ export const projectsData = [
         github:'https://github.com/THEYASHGAUR/QR-Code-Generator'
     },
     {
-        id:6,
+        id:7,
         image: Work7,
         title: 'Speech to Text Convertor',
         category: 'Frontend',
         demoLink: 'https://speech-to-text-convertor-yg.netlify.app/',
         github:'https://github.com/THEYASHGAUR/Speech-to-text-Convertor'
+    },
+    {
+        id:8,
+        image: Work8,
+        title: 'CLI Tool for QR Code Generator',
+        category: 'Backend',
+        demoLink: '#',
+        github: 'https://github.com/THEYASHGAUR/qr-code-generator-cli-hackathon'
+    },
+    {
+        id:9,
+        image: Work9,
+        title: 'Random Quotes Generator',
+        category: 'API Based',
+        demoLink: 'https://random-quote-generator-yg.netlify.app/',
+        github: 'https://github.com/THEYASHGAUR/Random-Quote-Generator'
     }
     
 ]
@@ -71,13 +89,16 @@ export const ProjectsNav = [
         name : 'All',
     },
     {
+        name:'API Based',
+    },
+    {
         name : 'Frontend',
     },
     {
-        name : 'API Based',
+        name : 'Backend',
     },
     {
-        name : 'UI / UX',
+        name : 'Full Stack',
     }
     
 
